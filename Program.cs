3b20fe4981fc484bd0618972e71ae92153c74ad4
Anil Beter <86671685+anilbeter@users.noreply.github.com>
2023-10-13 18:02:03 +0300

    if

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Do you want to go\na: left\nb: right?");
+string input = Console.ReadLine();
+if (input == "a")
+    Console.WriteLine("You went to left");
 
+if (input == "b") 
+    Console.WriteLine("You went to right");
